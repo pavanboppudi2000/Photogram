5cadedd2ee797da9686fa8bca20b252a1d4b7ea9
inspiron <pavanboppudi20122000@gmail.com>
2020-09-11 11:48:12 +0530

    Bug Fixes in edit and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,17 @@ class PostsController < ApplicationController
         @post.save
         redirect_to @post
     end
+    def update
+        @post=Post.find(params[:id])
+        @post.update(post_params)
+        redirect_to @post
+
+    end
+    def destroy
+        @post=Post.find(params[:id])
+        @post.destroy 
+        redirect_to posts_path
+    end
 
     private
      def post_params
